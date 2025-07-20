bf3a3316e589ab40a21f8564435b98b49fe3c455
jcarlossc <josecscosta@fac.pe.senac.br>
2025-07-20 20:29:15 -0300

    Docstring da classe ValidadorUrl adicionado.

--- src/validador/ValidadorUrl.py
+++ src/validador/ValidadorUrl.py
@@ -2,7 +2,23 @@ import re
 from src.validador.Validador import Validador
 
 class ValidadorUrl(Validador):
+    """
+    Estratégia concreta para validação de URLs.
+
+    Verifica se a string corresponde a um formato de URL.
+    """
     def validar(self, valor: str) -> bool:
+        """
+        Valida uma URL usando uma expressão regular.
+
+        Aceita URLs com ou sem 'http://' ou 'https://'.
+
+        Args:
+            value (str): A URL a ser validada.
+
+        Returns:
+            bool: True se a url for válida, False se inválida.
+        """
         try:
             regex = r"^(https?://)?([\da-z\.-]+)\.([a-z\.]{2,6})([/\w \.-]*)*\/?$"
             return re.fullmatch(regex, valor) is not None
