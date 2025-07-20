60b154a9bf84211ba39d97280c82f32cf1cf8e30
jcarlossc <josecscosta@fac.pe.senac.br>
2025-07-20 14:00:27 -0300

    Classe ValidadorEmail refatorada.

--- src/validador/ValidadorEmail.py
+++ src/validador/ValidadorEmail.py
@@ -3,5 +3,8 @@ from src.validador.Validador import Validador
 
 class ValidadorEmail(Validador):
     def validar(self, valor: str) -> bool:
-        regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-        return re.fullmatch(regex, valor) is not None
+        try:
+            regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+            return re.fullmatch(regex, valor) is not None
+        except Exception as e:
+            print(f"Erro. Avalidação de email falhou: {e}")
