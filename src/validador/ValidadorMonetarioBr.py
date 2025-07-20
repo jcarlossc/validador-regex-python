3af86d270680a70bd1c172dfaf7e1cd371757338
jcarlossc <josecscosta@fac.pe.senac.br>
2025-07-20 20:58:29 -0300

    Docstring da classe ValidadorMonetarioBr adicionado.

--- src/validador/ValidadorMonetarioBr.py
+++ src/validador/ValidadorMonetarioBr.py
@@ -2,7 +2,24 @@ import re
 from src.validador.Validador import Validador
 
 class ValidadorMonetarioBr(Validador):
+    """
+    Estratégia concreta para validação de valores monetários no formato brasileiro.
+
+    Aceita valores com vírgula para decimais e ponto para milhares.
+    """
     def validar(self, valor: str) -> bool:
+        """
+        Valida um valor monetário no formato brasileiro específico usando regex.
+
+        Exemplos de formatos válidos: "0,23", "1,23", "10,23", "100,23",
+        "1.235,20", "10.235,23", "R$ 1.235,20".
+
+        Args:
+            value (str): O valor monetário a ser validado.
+
+        Returns:
+            bool: True se o valor for válido, False se inválido.
+        """
         try:
             regex = r"^(R\$\s)?(\d{1,3}(?:\.\d{3})*|\d{1,3}),\d{2}$"
             return re.fullmatch(regex, valor) is not None
