0c2af9c05915ce7fe957f1906582ea94604e4879
jcarlossc <josecscosta@fac.pe.senac.br>
2025-07-20 19:50:45 -0300

    Docstring da classe ValidadorEmail adicionado.

--- src/validador/ValidadorEmail.py
+++ src/validador/ValidadorEmail.py
@@ -2,7 +2,23 @@ import re
 from src.validador.Validador import Validador
 
 class ValidadorEmail(Validador):
+    """
+    Estratégia concreta para validação de endereços de e-mail.
+
+    Utiliza uma expressão regular para verificar o formato de e-mail.
+    """
     def validar(self, valor: str) -> bool:
+        """
+        Valida um endereço de e-mail usando uma expressão regular.
+
+        A regex utilizada é simples e cobre a maioria dos formatos.
+
+        Args:
+            value (str): O endereço de e-mail a ser validado.
+
+        Returns:
+            bool: True se o email for válido, False se inválido.
+        """
         try:
             regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
             return re.fullmatch(regex, valor) is not None
