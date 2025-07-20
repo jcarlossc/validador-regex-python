c9592fe6d3eef1d51a19c5ab0433212de7a2c015
jcarlossc <josecscosta@fac.pe.senac.br>
2025-07-19 22:13:10 -0300

    Classe ValidadorIp modificada.

--- src/validador/ValidadorIp.py
+++ src/validador/ValidadorIp.py
@@ -1,7 +1,7 @@
 import re
 from src.validador.Validador import Validador
 
-class validadorIp(Validador):
+class ValidadorIp(Validador):
     def validar(self, valor: str) -> bool:
         regex = r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
         if not re.fullmatch(regex, valor):
