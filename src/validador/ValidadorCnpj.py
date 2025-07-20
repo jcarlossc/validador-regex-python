654c189a3b065a6a6d9e18adc02069bda8a8599b
jcarlossc <josecscosta@fac.pe.senac.br>
2025-07-20 15:43:58 -0300

    Classe ValidadorCnpj refatorada.

--- src/validador/ValidadorCnpj.py
+++ src/validador/ValidadorCnpj.py
@@ -3,7 +3,9 @@ from src.validador.Validador import Validador
 
 class ValidadorCnpj(Validador):
     def validar(self, valor: str) -> bool:
-        regex = r"^\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}$"
-        if not re.fullmatch(regex, valor):
-            return False
-        return True
+        try:
+            regex = r"^\d{2}\.?\d{3}\.?\d{3}\/?\d{4}-?\d{2}$"
+            return re.fullmatch(regex, valor) is not None
+        except (TypeError, re.error) as e:
+            print(f"Erro. A validação do cpf falhou: {e}")
+            return False 
