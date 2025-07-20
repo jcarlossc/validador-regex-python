297bec89f009de37583d2e8fd35d17341fc9500e
jcarlossc <josecscosta@fac.pe.senac.br>
2025-07-20 15:59:53 -0300

    Classe ValidadorData refatorada.

--- src/validador/ValidadorData.py
+++ src/validador/ValidadorData.py
@@ -3,7 +3,9 @@ from src.validador.Validador import Validador
 
 class ValidadorData(Validador):
     def validar(self, valor: str) -> bool:
-        regex = r"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$"
-        if not re.fullmatch(regex, valor):
-            return False
-        return True
+        try:
+            regex = r"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$"
+            return re.fullmatch(regex, valor) is not None
+        except (TypeError, re.error) as e:
+            print(f"Erro. A validação da data falhou: {e}")
+            return False 
