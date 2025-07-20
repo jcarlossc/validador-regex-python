4856b9a1cca17b4f2c8407533dc416395d42496d
jcarlossc <josecscosta@fac.pe.senac.br>
2025-07-20 16:02:32 -0300

    Classe ValidadorHora refatorada.

--- src/validador/ValidadorHora.py
+++ src/validador/ValidadorHora.py
@@ -3,7 +3,9 @@ from src.validador.Validador import Validador
 
 class ValidadorHora(Validador):
     def validar(self, valor: str) -> bool:
-        regex = r"^(?:2[0-3]|[01]?[0-9]):[0-5][0-9](?::[0-5][0-9])?$"
-        if not re.fullmatch(regex, valor):
-            return False
-        return True
+        try:
+            regex = r"^(?:2[0-3]|[01]?[0-9]):[0-5][0-9](?::[0-5][0-9])?$"
+            return re.fullmatch(regex, valor) is not None
+        except (TypeError, re.error) as e:
+            print(f"Erro. A validação da hora falhou: {e}")
+            return False 
