e9adb9b73cf9bd28c926466a07d8e4fad483f018
jcarlossc <josecscosta@fac.pe.senac.br>
2025-07-20 15:50:50 -0300

    Classe ValidadorIp refatorada.

--- src/validador/ValidadorIp.py
+++ src/validador/ValidadorIp.py
@@ -3,7 +3,9 @@ from src.validador.Validador import Validador
 
 class ValidadorIp(Validador):
     def validar(self, valor: str) -> bool:
-        regex = r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
-        if not re.fullmatch(regex, valor):
-            return False
-        return True
+        try:
+            regex = r"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+            return re.fullmatch(regex, valor) is not None
+        except (TypeError, re.error) as e:
+            print(f"Erro. A validação do IP falhou: {e}")
+            return False 
