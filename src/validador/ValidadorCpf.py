d23f466547cb4da87a68df1d020ec0eff9a28ac5
jcarlossc <josecscosta@fac.pe.senac.br>
2025-07-20 14:05:29 -0300

    Classe ValidadorCpf refatorada.

--- src/validador/ValidadorCpf.py
+++ src/validador/ValidadorCpf.py
@@ -3,5 +3,8 @@ from src.validador.Validador import Validador
 
 class ValidadorCpf(Validador):
     def validar(self, valor: str) -> bool:
-        regex = r"^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$"
-        return re.fullmatch(regex, valor) is not None
+        try:
+            regex = r"^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$"
+            return re.fullmatch(regex, valor) is not None
+        except Exception as e:
+            print(f"Erro. A validaçao de cpf falhou: {e}")
