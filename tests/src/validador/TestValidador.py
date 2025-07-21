c5e2e0581d7face81bcda63693a9884132b29e70
jcarlossc <josecscosta@fac.pe.senac.br>
2025-07-21 14:51:10 -0300

    Interface Validador testada.

--- tests/src/validador/TestValidador.py
+++ tests/src/validador/TestValidador.py
@@ -1,26 +1,34 @@
 import unittest
 from abc import ABC, abstractmethod
 
-# Suponha que este seja o seu código base
 class Validador(ABC):
+    """
+    Classe abstrata(Inteface).
+    """
     @abstractmethod
     def validar(self, valor: str) -> bool:
         pass
 
-# Classe concreta para teste.
 class ValidadorMock(Validador):
+    """
+    Classe concreta para teste(Mock).
+    """
     def validar(self, valor: str) -> bool:
         return valor == "testando"
 
 class TestValidador(unittest.TestCase):
 
     def test_validador_abstrato_nao_instanciavel(self):
-        # Tentativa de instanciar a classe abstrata.
+        """
+        Tentativa de instanciar a classe abstrata.
+        """
         with self.assertRaises(TypeError):
             validador = Validador()
 
     def test_validador_mock(self):
-        # Usa uma implementação concreta de Validador.
+        """
+        Usa uma implementação concreta de Validador.
+        """
         validador = ValidadorMock()
         self.assertTrue(validador.validar("testando"))
         self.assertFalse(validador.validar("outro"))
